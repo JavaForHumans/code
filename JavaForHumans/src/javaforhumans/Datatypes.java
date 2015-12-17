aa8894fb6fbdcbff03732a60f09a66092d83a3ee
Lincoln W Daniel <lwdthe1@gmail.com>
2015-12-17 07:39:51 -0500

    Java for humans up to Variables

---
+++ JavaForHumans/src/javaforhumans/Datatypes.java
@@ -0,0 +1,60 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package javaforhumans;
+
+/**
+ *
+ * @author lwdthe1
+ */
+public class Datatypes {
+
+    /**
+     * @params args
+     */
+    public static void main(String[] args) {
+        /*Declare and instantiate nine different variables to store our account's nine different data points*/
+
+        /*declare an int variable to store our account identifier*/
+        int id = 1234567828;
+
+        /*declare a double variable to store our account balance*/
+        double balance = 1301.41;
+
+        /*declare a int variable to store pass code*/
+        int passCode = 9889;
+
+        /*declare a boolean variable to store our account's active status*/
+        boolean isActive = true;
+
+        /*declare a String variable to store our account owner's name*/
+        String ownerName = "Lincoln W Daniel";
+
+        /*declare a String variable to store our account owner's address*/
+        String ownerAddress = "325 JavaPlace, UnitedJavas 19081";
+
+        /*declare an int variable to store our account owner's birth year*/
+        int ownerBirthYear = 1993;
+
+        /*declare a String variable to store our account owner's birth month*/
+        String ownerBirthMonth = "May";
+
+        /*declare an int variable to store our account owner's birth day*/
+        int ownerBirthDay = 26;
+        
+        /*print out our bank account's balance*/
+        System.out.println("Our bank account's unique identifier (id): " + id);
+
+        /*print out our bank account's balance*/
+        System.out.println("Our bank account's balance: " + balance);
+
+        /*add 600 dollars and 50 cents to our bank account*/
+        balance = balance + 600.50;
+
+        System.out.println("");
+        /*print out our new bank account balance*/
+        System.out.println("Our account's new balance: $" + balance);
+    }
+}
---
+++ JavaForHumans/src/javaforhumans/Variables.java
@@ -0,0 +1,32 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package javaforhumans;
+
+/**
+ *
+ * @author lwdthe1
+ */
+public class Variables {
+    public static void main(String[] args) {
+        /*This is a multi-line comment. It won't be compiled by the 
+        compiler*/
+
+        /*Declare and instantiate a single variable to store    
+        information about our bank account*/
+        String bankAccount = "identifier: 1234567828; "
+        + "balance: 1301.78; passcode: 9889; active: true; " 
+        + "owner's name: Lincoln Daniel; " 
+        + "owner's address: 325 JavaPlace, UnitedJavas 19081; "
+        + "owner's birthday: May 26, 1993";
+        
+
+        /*print out the data stored in the bankAccount String 
+        variable.
+        All the information in the bankAccount variable will be 
+        printed.*/
+        System.out.println("Our bank account: " + bankAccount);
+    }
+}
