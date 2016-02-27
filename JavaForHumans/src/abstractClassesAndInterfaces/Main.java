8840a81059f4ec250e8414aafe4ddb32f27805fa
Lincoln W Daniel <lwdthe1@gmail.com>
2016-02-27 17:04:03 -0500

    Update Main.java

--- JavaForHumans/src/abstractClassesAndInterfaces/Main.java
+++ JavaForHumans/src/abstractClassesAndInterfaces/Main.java
@@ -1,36 +1,28 @@
-package abstractClassesAndInterfaces;
-
-/**
- * Created by Lincoln W Daniel on 1/17/2016.
- */
 public class Main {
     public static void main(String[] args){
         Archer niceArcher = new Archer("Tom");
-        Human meanArcher = new Archer("Stacy");
-        Character modernArcher = new Archer("Tabitha");
+        Human modernArcher = new Archer("Stacy");
 
-        Dog wildDog = new Dog("Hunter", meanArcher);
+        Dog modernDog = new Dog("Hunter", modernArcher);
         Pet petDog = new Dog("Buddy", niceArcher);
-        Character modernDog = new Dog("Java", meanArcher);
 
         System.out.println("\nStart Game\n");
-        meanArcher.attack(niceArcher);
-        niceArcher.getPet().attack(meanArcher);
-        meanArcher.attack(niceArcher.getPet());
+        modernArcher.attack(niceArcher);
+        niceArcher.getPet().attack(modernArcher);
+        modernArcher.attack(niceArcher.getPet());
+        modernDog.attack(niceArcher);
+        petDog.attack(modernArcher);
     }
     /*Prints:
-        Tom: Found 5 arrows ...
-        Stacy: Found 3 arrows ...
-        Tabitha: Found 6 arrows ...
+        Tom: Found 2 arrows ...
+        Stacy: Found 6 arrows ...
         Stacy: I have a new pet. Hi Hunter!
         Hunter: Wolf Wolf!
         Tom: I have a new pet. Hi Buddy!
         Buddy: Wolf Wolf!
-        Stacy: I have a new pet. Hi Java!
-        Java: Wolf Wolf!
-
+        
         Start Game
-
+        
         Stacy: Attacking Tom with my arrows!
         Tom: I've been hit. My health now = 90.0
         Buddy: Biting Stacy
@@ -38,5 +30,9 @@ public class Main {
         Stacy: Attacking Buddy with my arrows!
         Buddy: Wolf Wolf!
         Buddy: Health now = 90.0
+        Hunter: Biting Tom
+        Tom: I've been hit. My health now = 89.0
+        Buddy: Biting Stacy
+        Stacy: I've been hit. My health now = 98.0
      */
 }
