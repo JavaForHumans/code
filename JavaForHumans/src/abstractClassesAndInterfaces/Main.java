8bc636cfdf1552353e9f1a9702a75e8e7aef44db
Lincoln W Daniel <lwdthe1@gmail.com>
2016-02-27 16:52:50 -0500

    Update Main.java

--- JavaForHumans/src/abstractClassesAndInterfaces/Main.java
+++ JavaForHumans/src/abstractClassesAndInterfaces/Main.java
@@ -1,19 +1,42 @@
 package abstractClassesAndInterfaces;
 
 /**
- * Created by lwdthe1 on 1/17/2016.
+ * Created by Lincoln W Daniel on 1/17/2016.
  */
 public class Main {
     public static void main(String[] args){
-        Human niceArcher = new Archer("Stacy");
-        Pet pet = new Dog("Buddy", niceArcher);
-        Archer meanArcher = new Archer("Tom");
+        Archer niceArcher = new Archer("Tom");
+        Human meanArcher = new Archer("Stacy");
+        Character modernArcher = new Archer("Tabitha");
 
-        System.out.println();
+        Dog wildDog = new Dog("Hunter", meanArcher);
+        Pet petDog = new Dog("Buddy", niceArcher);
+        Character modernDog = new Dog("Java", meanArcher);
+
+        System.out.println("\nStart Game\n");
         meanArcher.attack(niceArcher);
-        System.out.println();
         niceArcher.getPet().attack(meanArcher);
-        System.out.println();
         meanArcher.attack(niceArcher.getPet());
     }
+    /*Prints:
+        Tom: Found 5 arrows ...
+        Stacy: Found 3 arrows ...
+        Tabitha: Found 6 arrows ...
+        Stacy: I have a new pet. Hi Hunter!
+        Hunter: Wolf Wolf!
+        Tom: I have a new pet. Hi Buddy!
+        Buddy: Wolf Wolf!
+        Stacy: I have a new pet. Hi Java!
+        Java: Wolf Wolf!
+
+        Start Game
+
+        Stacy: Attacking Tom with my arrows!
+        Tom: I've been hit. My health now = 90.0
+        Buddy: Biting Stacy
+        Stacy: I've been hit. My health now = 99.0
+        Stacy: Attacking Buddy with my arrows!
+        Buddy: Wolf Wolf!
+        Buddy: Health now = 90.0
+     */
 }
